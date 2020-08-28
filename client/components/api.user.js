3c6e93ff1d31034d93284875b5ae32c152bf8238
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-08-28 17:05:09 -0500

    added signin and signout fetch functions

--- client/components/api.user.js
+++ client/components/api.user.js
@@ -14,4 +14,32 @@ const createUser = async (newUser) => {
     }
 };
 
-export { createUser }
+const signIn = async (user) => {
+    try {
+        let response = await fetch('/auth/signin/', {
+            method: 'POST',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(user)
+        })
+        return await response.json();
+    } catch(err) {
+        console.log(err)
+    }
+};
+
+const signOut = async () => {
+    try {
+      let response = await fetch('/auth/signout/', { 
+          method: 'GET' }
+        )
+      return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+};
+
+export { createUser, signIn, signOut }
