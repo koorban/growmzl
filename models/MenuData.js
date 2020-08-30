29a18375ebc61cb5cbbd9641e4fc67b74a98899b
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-08-30 13:04:19 -0500

    reorganized MenuData and removed signin url

--- models/MenuData.js
+++ models/MenuData.js
@@ -1,32 +1,28 @@
 const MenuData = [
-    {
-      title: "Contact",
-      url: "/contact"
-    },
-    {
-      title: "All",
-      url: "/products/list/all"
-    },
-    {
-      title: "Strawberries",
-      url: "/products/by/Strawberry"
-    },
-    {
-      title: "Fresh Greens",
-      url: "/products/by/Greens"
-    },
-    {
-      title: "Basil",
-      url: "/products/by/Basil"
-    },
-    {
-      title: "Sign Up",
-      url: "/signup"
-    },
-    {
-      title: "Sign In",
-      url: "/signin"
-    }
+  {
+    title: "Strawberries",
+    url: "/products/by/Strawberry"
+  },
+  {
+    title: "Fresh Greens",
+    url: "/products/by/Greens"
+  },
+  {
+    title: "Basil",
+    url: "/products/by/Basil"
+  },
+  {
+    title: "All",
+    url: "/products/list/all"
+  },
+  {
+    title: "Contact",
+    url: "/contact"
+  },
+  {
+    title: "Sign In",
+    url: "/signin"
+  },
 ];
 
 export default MenuData
