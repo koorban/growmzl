857dbf1af1f68703874ca629e6e03623f81ed919
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-08-30 15:47:44 -0500

    changed background color and font color

--- client/core/MenuFlex.js
+++ client/core/MenuFlex.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import  menuData from '../../models/MenuData';
 import Button from '@material-ui/core/Button';
 import Link  from '@material-ui/core/Link';
 import { Link as RouterLink } from 'react-router-dom';
+import  menuData from '../../models/MenuData';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
-        margin: 4,
+        margin: 3,
       },
       display: 'flex',
       justifyContent: 'space-around',
       flexWrap: 'wrap'
     },
-    
+    button: {
+      backgroundColor: '#083818',
+    },
+    link: {
+      color: '#fff'
+    }
 }));
 
 export default function MenuFlex() {
@@ -22,17 +27,17 @@ export default function MenuFlex() {
 
   return (
     <div className={classes.root}>
-      <Button variant="contained" color="primary">
-          <Link to="/" component={RouterLink} color="inherit">
-              HOME
+      <Button variant="contained" className={classes.button}>
+          <Link to="/" component={RouterLink} className={classes.link}>
+            HOME
           </Link>
       </Button>
       
       {menuData.map((data) => {
       return (
       <>
-          <Button variant="contained" color="primary">
-              <Link component={RouterLink} to={data.url} color="inherit">
+          <Button variant="contained" className={classes.button}>
+              <Link component={RouterLink} to={data.url} className={classes.link}>
                 {data.title}
               </Link>
           </Button>
