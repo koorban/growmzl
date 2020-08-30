7a3de37dace7c345457a9d336c80dcea89d1dbff
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-08-30 15:43:48 -0500

    add background pic w/ css

--- client/core/CompanyTitle.js
+++ client/core/CompanyTitle.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import backdropImage from '../../dist/backdroplogo.jpg';
 
 const useStyles = makeStyles({
     root: {
+        position: 'absolute',
+        bottom: 3,
+        width: "100%",
+        paddingTop: 20,
+        paddingBottom: 20,
         textAlign: 'center',
-        borderRadius: 4,
-        border: '2px solid black',
-        paddingBottom: 10,
-        paddingTop: 10,
-        marginLeft: 80,
-        marginRight: 80,
-        marginBottom: 50,
-        fontStyle: 'oblique',
-        fontWeight: 'bold'
-    }
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    image: {
+        minHeight: 240,
+        maxHeight: 240,
+        width: "100%"
+    },
+    wrapper: {
+        position: 'relative',
+        height: "100%",
+        marginBottom: 60
+    },
 });
 
 export default function CompanyTitle() {
     const classes = useStyles()
     return (
-        <div className={classes.root}>
-            <Typography variant="h1" component="h2">
-                Grow Mzl
-            </Typography>
-            <Typography variant="h5">
-                Est. 2020
-            </Typography>
-        </div>
+        <>
+            <div className={classes.wrapper}>
+                <img src={backdropImage} className={classes.image}/>
+                <div className={classes.root}>
+                    <Typography variant="h1" component="h2">
+                        Grow Mzl
+                    </Typography>
+                    <Typography variant="h5">
+                        Est. 2020
+                    </Typography>
+                </div>
+            </div>
+        </>
     );
 };
