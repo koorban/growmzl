dd456a06b6bfcd8946086dc7cb6cd753baf6027f
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-09-07 16:14:56 -0500

    org

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -3,30 +3,29 @@ import { Route, Switch } from 'react-router-dom';
 
 //components
 import MainAppBar from './core/AppBar';
-import Footer from './core/Footer';
 import CompanyTitle from './core/CompanyTitle';
-import Home from './components/Home';
+import HomePage from './components/Pages/Home-Page';
+import CategoryPage from './components/Pages/Category-Page';
+import AllProductsPage from './components/Pages/All-Products-Page';
+import ProductDetailsPage from './components/Pages/Product-Details-Page';
+import PrivateRoute from './core/PrivateRoute';
 import Upload from './components/Upload';
-import Products from './components/Products';
-import AllProducts from './components/AllProducts';
-import ProductInfo from './components/ProductInfo';
 import Contact from './components/Contact';
-import About from './components/About';
 import SignIn from './components/SignIn';
 
 export default function MainRouter(){
-    return (<div>
+  return (
+    <div>
       <MainAppBar/>
       <CompanyTitle/>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/signin" component={SignIn}/>
-        <Route exact path="/upload" component={Upload}/>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/products/list/all" component={AllProductsPage}/>
+        <Route path="/products/by/:category" component={CategoryPage}/>
+        <Route path="/products/product/:productId/" component={ProductDetailsPage}/>
+        <PrivateRoute exact path='/upload'  component={Upload}/>
         <Route exact path="/contact" component={Contact}/>
-        <Route exact path="/about" component={About}/>
-        <Route exact path="/products/list/all" component={AllProducts}/>
-        <Route path="/products/product/:productId/" component={ProductInfo}/>
-        <Route path="/products/by/:category" component={Products}/>
+        <Route exact path="/signin" component={SignIn}/>
       </Switch>
     </div>
   );
