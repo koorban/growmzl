7941c9f07d4e1f35ca47b5bca2e4ced95aa51128
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-09-08 11:37:46 -0500

    added components and styling

--- client/core/Footer.js
+++ client/core/Footer.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import FooterSignup from '../components/Page-Components/FooterSignup';
+import FooterLinks from '../components/Page-Components/FooterLinks';
+import FooterHelp from '../components/Page-Components/FooterHelp';
+import Socials from '../components/Page-Components/Socials';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgba(8, 56, 24, 0.9)',
+    color: '#cccccc',
+    minHeight: 500,
+    alignItems: 'center'
   },
-  toolbar: {
-    minHeight: 40,
-    alignItems: 'flex-start',
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(3)
+  signup: {
+    marginTop: 30,
+    marginBottom: 160
   },
-  title: {
-    flexGrow: 1,
-    alignSelf: 'flex-end',
-  },
-  appBar: {
-    top: 'auto',
-    bottom: 0,
-    backgroundColor: '#B0B0B0'
+  links: {
+    marginBottom: 20
   }
 }));
 
@@ -34,22 +29,23 @@ export default function ProminentAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Typography className={classes.title} variant="body2">
-              © Copyright 2020
-          </Typography>
-          <Typography className={classes.title}>
-              <FacebookIcon/>
-          </Typography>
-          <Typography className={classes.title}>
-              <TwitterIcon/>
-          </Typography>
-          <Typography className={classes.title}>
-              <InstagramIcon/>
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      
+      <div className={classes.signup}>
+        <FooterSignup/>
+      </div>
+
+      <div className={classes.links}>
+        <FooterLinks/>
+      </div>
+
+      <div className={classes.item}>
+        <FooterHelp/>
+      </div>
+
+      <div>
+        <Socials/>
+      </div>
+
     </div>
   );
 };
