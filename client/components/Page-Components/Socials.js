3e0953cc035b1ebceef928bf0d6f1151e41436b6
Kevin Lyle <kevin.341911507@ucaldas.edu.co>
2020-09-08 22:54:49 -0500

    footer component

---
+++ client/components/Page-Components/Socials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+export default function Socials() {
+
+    return (
+        <>
+            <FacebookIcon/>
+            <TwitterIcon/>
+            <InstagramIcon/>
+        </>
+    );
+};
